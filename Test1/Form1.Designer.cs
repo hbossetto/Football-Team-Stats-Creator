44e541b8c27260e85806620ce83111fd3a624fd9
mario <mario@DESKTOP-OIDI4OL>
2022-03-23 16:33:37 -0500

    Add project files.

---
+++ Test1/Form1.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace Test1
+{
+    partial class ramsPictureBox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ramsPictureBox));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.teamNameTextBox = new System.Windows.Forms.TextBox();
+            this.teamCityTextBox = new System.Windows.Forms.TextBox();
+            this.gamesWonTextBox = new System.Windows.Forms.TextBox();
+            this.gamesLossTextBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.cityNameLabel = new System.Windows.Forms.Label();
+            this.gamesPlayedLabel = new System.Windows.Forms.Label();
+            this.winningPercentageLabel = new System.Windows.Forms.Label();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(18, 18);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(422, 311);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(468, 45);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Team Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(468, 89);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Team City:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(468, 134);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Games Won:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(468, 185);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(103, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Games Loss:";
+            // 
+            // teamNameTextBox
+            // 
+            this.teamNameTextBox.Location = new System.Drawing.Point(597, 45);
+            this.teamNameTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.teamNameTextBox.Name = "teamNameTextBox";
+            this.teamNameTextBox.Size = new System.Drawing.Size(148, 26);
+            this.teamNameTextBox.TabIndex = 5;
+            // 
+            // teamCityTextBox
+            // 
+            this.teamCityTextBox.Location = new System.Drawing.Point(597, 86);
+            this.teamCityTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.teamCityTextBox.Name = "teamCityTextBox";
+            this.teamCityTextBox.Size = new System.Drawing.Size(148, 26);
+            this.teamCityTextBox.TabIndex = 6;
+            // 
+            // gamesWonTextBox
+            // 
+            this.gamesWonTextBox.Location = new System.Drawing.Point(597, 134);
+            this.gamesWonTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.gamesWonTextBox.Name = "gamesWonTextBox";
+            this.gamesWonTextBox.Size = new System.Drawing.Size(148, 26);
+            this.gamesWonTextBox.TabIndex = 7;
+            // 
+            // gamesLossTextBox
+            // 
+            this.gamesLossTextBox.Location = new System.Drawing.Point(597, 185);
+            this.gamesLossTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.gamesLossTextBox.Name = "gamesLossTextBox";
+            this.gamesLossTextBox.Size = new System.Drawing.Size(148, 26);
+            this.gamesLossTextBox.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(123, 378);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(160, 20);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Team City and Name:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(129, 428);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(155, 20);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Total Games Played:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(123, 480);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(156, 20);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Winning Percentage:";
+            // 
+            // cityNameLabel
+            // 
+            this.cityNameLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.cityNameLabel.Location = new System.Drawing.Point(296, 378);
+            this.cityNameLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.cityNameLabel.Name = "cityNameLabel";
+            this.cityNameLabel.Size = new System.Drawing.Size(274, 34);
+            this.cityNameLabel.TabIndex = 12;
+            this.cityNameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // gamesPlayedLabel
+            // 
+            this.gamesPlayedLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.gamesPlayedLabel.Location = new System.Drawing.Point(292, 428);
+            this.gamesPlayedLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.gamesPlayedLabel.Name = "gamesPlayedLabel";
+            this.gamesPlayedLabel.Size = new System.Drawing.Size(149, 34);
+            this.gamesPlayedLabel.TabIndex = 13;
+            this.gamesPlayedLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // winningPercentageLabel
+            // 
+            this.winningPercentageLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.winningPercentageLabel.Location = new System.Drawing.Point(292, 472);
+            this.winningPercentageLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.winningPercentageLabel.Name = "winningPercentageLabel";
+            this.winningPercentageLabel.Size = new System.Drawing.Size(149, 34);
+            this.winningPercentageLabel.TabIndex = 14;
+            this.winningPercentageLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(128, 575);
+            this.calculateButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(124, 65);
+            this.calculateButton.TabIndex = 15;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(292, 575);
+            this.clearButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(124, 65);
+            this.clearButton.TabIndex = 16;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(447, 575);
+            this.exitButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(124, 65);
+            this.exitButton.TabIndex = 17;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // ramsPictureBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(783, 692);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.winningPercentageLabel);
+            this.Controls.Add(this.gamesPlayedLabel);
+            this.Controls.Add(this.cityNameLabel);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.gamesLossTextBox);
+            this.Controls.Add(this.gamesWonTextBox);
+            this.Controls.Add(this.teamCityTextBox);
+            this.Controls.Add(this.teamNameTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "ramsPictureBox";
+            this.Text = "Football Team Stats";
+            this.Load += new System.EventHandler(this.ramsPictureBox_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox teamNameTextBox;
+        private System.Windows.Forms.TextBox teamCityTextBox;
+        private System.Windows.Forms.TextBox gamesWonTextBox;
+        private System.Windows.Forms.TextBox gamesLossTextBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label cityNameLabel;
+        private System.Windows.Forms.Label gamesPlayedLabel;
+        private System.Windows.Forms.Label winningPercentageLabel;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ Test1/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test1
+{
+    public partial class ramsPictureBox : Form
+    {
+        public ramsPictureBox()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Declare all variables and strings
+                string teamName;
+                string teamCity;
+                int gamesWon;
+                int gamesLoss;
+                string cityName;
+                int gamesPlayed;
+                double winningPercentage;
+
+                // Input Data
+                teamName = teamNameTextBox.Text;
+                teamCity = teamCityTextBox.Text;
+                gamesWon = int.Parse(gamesWonTextBox.Text);
+                gamesLoss = int.Parse(gamesLossTextBox.Text);
+
+                // Combine City and Name
+                cityName = "The" + " " + teamCity + " " + teamName;
+
+                // Output the combination
+                cityNameLabel.Text = cityName;
+
+                // Calculate total games played
+                gamesPlayed = gamesWon + gamesLoss;
+
+                // Output total games
+                gamesPlayedLabel.Text = gamesPlayed.ToString();
+
+                // Calculate winning percentage
+                winningPercentage = (double)gamesWon / (double)gamesPlayed;
+
+                // Output winning percentage
+                winningPercentageLabel.Text = winningPercentage.ToString("p1");
+            }
+            catch (Exception ex)
+            {
+                // Display the default error message.
+                MessageBox.Show(ex.Message);
+
+            }
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            // Clearing all text boxs and labels.
+            teamNameTextBox.Text = "";
+            teamCityTextBox.Text = "";
+            gamesWonTextBox.Text = "";
+            gamesLossTextBox.Text = "";
+            cityNameLabel.Text = "";
+            gamesPlayedLabel.Text = "";
+            winningPercentageLabel.Text = "";
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            //Close the form.
+            this.Close();
+        }
+
+        private void ramsPictureBox_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
